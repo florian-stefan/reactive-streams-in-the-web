b9cf6aad26dc4a03f45e58156c048bc28bf91495
fstefan <fstefan@ebay.com>
2017-09-30 14:23:51 +0200

    Uses Reactive Thymeleaf to implement Progressive HTML Rendering

--- big-pipe/src/main/java/de/mobile/bigpipe/Application.java
+++ big-pipe/src/main/java/de/mobile/bigpipe/Application.java
@@ -2,14 +2,28 @@ package de.mobile.bigpipe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.thymeleaf.spring5.ISpringWebFluxTemplateEngine;
+import org.thymeleaf.spring5.view.reactive.ThymeleafReactiveViewResolver;
 
 @SpringBootApplication
+@EnableConfigurationProperties(ThymeleafProperties.class)
 public class Application {
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
+  }
+
+  @Bean
+  public ThymeleafReactiveViewResolver thymeleafReactiveViewResolver(ISpringWebFluxTemplateEngine templateEngine) {
+    final ThymeleafReactiveViewResolver viewResolver = new ThymeleafReactiveViewResolver();
+    viewResolver.setTemplateEngine(templateEngine);
+    viewResolver.setOrder(1);
+    viewResolver.setResponseMaxChunkSizeBytes(8192);
+    return viewResolver;
   }
 
   @Bean
--- big-pipe/src/main/java/de/mobile/bigpipe/WidgetController.java
+++ big-pipe/src/main/java/de/mobile/bigpipe/WidgetController.java
@@ -1,46 +1,24 @@
 package de.mobile.bigpipe;
 
-import de.mobile.bigpipe.WidgetService.Pagelet;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.thymeleaf.context.Context;
-import org.thymeleaf.spring5.SpringTemplateEngine;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
+import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
 
-import static java.lang.String.format;
-import static org.springframework.http.MediaType.TEXT_HTML_VALUE;
-
-@RestController
+@Controller
 public class WidgetController {
 
   private final WidgetService widgetService;
-  private final SpringTemplateEngine templateEngine;
 
-  public WidgetController(final WidgetService widgetService,
-                          final SpringTemplateEngine templateEngine) {
+  public WidgetController(final WidgetService widgetService) {
     this.widgetService = widgetService;
-    this.templateEngine = templateEngine;
   }
 
-  @GetMapping(value = "/widgets", produces = TEXT_HTML_VALUE)
-  public Flux<String> widgets() {
-    String template = templateEngine.process("widgets", new Context());
+  @GetMapping(value = "/widgets")
+  public String widgets(Model model) {
+    model.addAttribute("pagelets", new ReactiveDataDriverContextVariable(widgetService.loadPagelets(), 1));
 
-    return Flux.concat(
-      Mono.just(template.replace("</body>", "").replace("</html>", "")),
-      widgetService.loadPagelets().map(this::wrapAsJavaScriptFunction),
-      Mono.just("</body></html>")
-    );
-  }
-
-  private String wrapAsJavaScriptFunction(Pagelet pagelet) {
-    return format("<script>(function(){" +
-      "var element = document.getElementById(\"%s\");" +
-      "if(element) {" +
-      "element.innerHTML = \"%s\";" +
-      "}" +
-      "})();</script>", pagelet.getName(), pagelet.getContent());
+    return "widgets";
   }
 
 }
